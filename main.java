0393fa1efcfbaa182d90848ef02bb22c338955a7
Sanjeet-GitHub <sanjeet-github@gmail.com>
2022-03-17 00:52:55 +0530

    first commit

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class main{
+    public static void main(String[] args) {
+        System.out.println("What is your name?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello"+name);
+
+
+    }
+}
